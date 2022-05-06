40f7ba5a4f020b1190afa657114a481d1c1a3b55
Matthew Guizar <matthewrguizar@gmail.com>
2022-05-06 07:13:42 -0700

    Opened file and print contents to console part 2

--- src/com/devmountain/Main.java
+++ src/com/devmountain/Main.java
@@ -1,7 +1,40 @@
 package com.devmountain;
 
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         System.out.println("hi");
+
+        //txt files
+        File cases = new File("./data/txt/Case.txt");
+        File cpus = new File("./data/txt/CPU.txt");
+        File cpuCooler = new File ("./data/txt/GPU.txt");
+
+        //csv files
+        File motherboard = new File ("./data/csv/motherboard.csv");
+        File psu = new File("./data/csv/psu.csv");
+        File ram = new File ("./data/csv/ram.csv");
+        File storage = new File ("./data/csv/storage.csv");
+        openFileAndPrint(cases);
+
     }
+
+    public static void openFileAndPrint (File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        while((line = bufferedReader.readLine()) != null){
+            String[] values = line.split("\\|");
+            System.out.println(Arrays.toString(values));
+        }
+
+    }
+
+
+
 }
